419b90f24cd469b95f9021b02bf0c7aca5edc425
Vitor <vitor.espinozadg@gmail.com>
2024-06-11 23:51:34 -0300

    feat(WB-019): Creating the models needed to map a doctor

---
+++ src/app/core/shared/models/address.model.ts
@@ -0,0 +1,10 @@
+export interface Address
+{
+			addressId: string,
+			street: string,
+			number: string,
+			neighborhood: string,
+			city: string,
+			state: string,
+			zipCode: string,
+	}
---
+++ src/app/core/units/models/unit.model.ts
@@ -0,0 +1,6 @@
+import { Address } from "cluster";
+
+export interface Unit {
+		address: Address,
+		displayName: string,
+}
