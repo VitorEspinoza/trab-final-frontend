96987c60dd460085bc2e9ca55799f25b97380625
Vitor <vitor.espinozadg@gmail.com>
2024-06-13 00:02:40 -0300

    feat(WB-023): Creating specialties list component

---
+++ src/app/core/specialties/specialties-list/specialties-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-specialties-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './specialties-list.component.html',
+  styleUrl: './specialties-list.component.scss'
+})
+export class SpecialtiesListComponent {
+
+}
