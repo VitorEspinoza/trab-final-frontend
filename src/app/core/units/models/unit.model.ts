fb84658dc149f6b6e5ba09e4456205c2e5939cb4
Vitor <vitor.espinozadg@gmail.com>
2024-06-12 21:26:09 -0300

    feat(WB-021): Updating unit model

--- src/app/core/units/models/unit.model.ts
+++ src/app/core/units/models/unit.model.ts
@@ -1,6 +1,10 @@
 import { Address } from "cluster";
+import { Specialty } from "../../specialties/models/specialty.model";
 
 export interface Unit {
+    unitId: string,
 		address: Address,
 		displayName: string,
+    name: string,
+    specialties: Specialty[]
 }
