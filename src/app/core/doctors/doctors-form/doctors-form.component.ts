5b803fa59ec028a8acb5413fd68d6f05303b27bb
Vitor <vitor.espinozadg@gmail.com>
2024-06-17 01:12:27 -0300

    feat(WB-025): creating doctor form component

---
+++ src/app/core/doctors/doctors-form/doctors-form.component.ts
@@ -0,0 +1,14 @@
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-doctors-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './doctors-form.component.html',
+  styleUrl: './doctors-form.component.scss'
+})
+export class DoctorsFormComponent {
+
+
+}
