672ae03df575a1c5f45c76da98e49cc436da3563
Vitor <vitor.espinozadg@gmail.com>
2024-06-17 12:04:05 -0300

    feat(WB-026): Adding doctors view methods and template

--- src/app/core/doctors/doctors-view/doctors-view.component.ts
+++ src/app/core/doctors/doctors-view/doctors-view.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Doctor } from '../models/doctor.model';
+import { NotificationService } from '../../shared/services/notification.service';
+import { DoctorsFormService } from '../shared/doctors-form.service';
+import { CommonModule } from '@angular/common';
+import { CepPipe } from '../../shared/pipes/cep.pipe';
+import { MatChipsModule } from '@angular/material/chips';
 
 @Component({
   selector: 'app-doctors-view',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, CepPipe, MatChipsModule],
   templateUrl: './doctors-view.component.html',
   styleUrl: './doctors-view.component.scss'
 })
-export class DoctorsViewComponent {
+
+export class DoctorsViewComponent implements OnInit{
+  route = inject(ActivatedRoute);
+  notificationService = inject(NotificationService);
+  doctorsFormService = inject(DoctorsFormService);
+  doctorId = this.route.snapshot.paramMap.get('id');
+  doctor;
+  ngOnInit(): void {
+    this.getDoctor();
+  }
+
+  getDoctor() {
+     this.doctorsFormService.getDoctor(this.doctorId).subscribe({
+        next: (doctor: Doctor) => {
+          this.doctor = doctor;
+        },
+        error: (error) => {
+          this.notificationService.showNotification({title: 'Error', message: 'Houve um erro ao carregar informações do médico', type: 'error'});
+        }
+      });
+  }
 
 }
