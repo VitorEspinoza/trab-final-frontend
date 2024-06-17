1a9ed95737a36859bd9d92e48c54070ae5757ec9
Vitor <vitor.espinozadg@gmail.com>
2024-06-17 00:47:45 -0300

    feat: creating cep pipe

---
+++ src/app/core/shared/pipes/cep.pipe.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { CepPipe } from './cep.pipe';
+
+describe('Pipe: Cepe', () => {
+  it('create an instance', () => {
+    let pipe = new CepPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
