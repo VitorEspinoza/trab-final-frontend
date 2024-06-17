fe19a261a9f2fd94af48fa732cad0b3e6f4ff662
Vitor <vitor.espinozadg@gmail.com>
2024-06-17 17:21:27 -0300

    feat(WB-029): adding methods in unit form service

--- src/app/core/units/shared/units-form.service.ts
+++ src/app/core/units/shared/units-form.service.ts
@@ -12,4 +12,27 @@ export class UnitsFormService{
   getUnit(id: string) {
     return this.http.get(`http://localhost:3000/units/${id}`).pipe(take(1));
   }
+
+  saveUnit(unit) {
+    return this.http.post('http://localhost:3000/units', unit).pipe(take(1));
+  }
+
+  updateUnit(unitId, unit) {
+    return this.http.put(`http://localhost:3000/units/${unitId}`, unit).pipe(take(1));
+  }
+
+  getUnits() {
+    return this.http.get('http://localhost:3000/units').pipe(take(1));
+  }
+
+  mountBody(unit) {
+    const specialties = unit.specialties.map(specialty => {
+      return {specialtyId: specialty, isPrincipalSpecialty: unit.principalSpecialties.includes(specialty)};
+    });
+    delete unit.principalSpecialties;
+    return {
+      ...unit,
+      specialties: specialties,
+    };
+  }
 }
