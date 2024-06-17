bd171eff71ad0f2afeeb9cf737cd7427babb512a
Vitor <vitor.espinozadg@gmail.com>
2024-06-17 05:43:20 -0300

    feat(WB-026): creating doctors view component

---
+++ src/app/core/doctors/doctors-view/doctors-view.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-doctors-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './doctors-view.component.html',
+  styleUrl: './doctors-view.component.scss'
+})
+export class DoctorsViewComponent {
+
+}
